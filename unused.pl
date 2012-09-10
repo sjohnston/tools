c6aa881b07bf940f8a4a0584124c77f7f25488c4
Stuart A Johnston <saj_git@thecommune.net>
2012-09-10 15:05:10 -0400

    initial import

---
+++ unused.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use v5.12;
+
+use PPI::Document;
+use PPI::Dumper;
+use PPI::Find;
+use Data::Dumper;
+
+my %import;
+my $doc = PPI::Document->new($ARGV[0]);
+
+my $use = $doc->find( sub { $_[1]->isa('PPI::Statement::Include') } );
+foreach my $u (@$use) {
+    my $node = $u->find_first('PPI::Token::QuoteLike::Words');
+    next unless $node;
+    $import{$u->module} //= [];
+    push $import{$u->module}, $node->literal;
+}
+
+my $words = $doc->find( sub { $_[1]->isa('PPI::Token::Word') } );
+
+
+my @words = map { $_->content } @$words;
+
+my %words;
+@words{ @words } = 1;
+
+foreach my $u (keys %import) {
+    say $u;
+    foreach my $w (@{$import{$u}}) {
+        if (exists $words{$w}) {
+            say "\t- Found $w";
+        }
+        else {
+            say "\t- Can't find $w";
+        }
+    }
+}
